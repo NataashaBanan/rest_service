767bc393d98df174dcf2deb4e5276300874bc69e
NataashaBanan <49069370+NataashaBanan@users.noreply.github.com>
2020-03-06 21:33:43 +0500

    Initial commit

---
+++ src/main/java/my_code/rest_service/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package my_code.rest_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
